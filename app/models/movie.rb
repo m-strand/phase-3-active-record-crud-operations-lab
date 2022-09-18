04d24b4967100fb612d0851bfcb7ebbf75f77e7c
Brooke <brooke.strand007@gmail.com>
2022-09-18 16:32:37 -0400

    commit tree ae7795163b26e1475f9a18c2b100197bea0da243

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,45 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        movie = Movie.create(title: title)
+    end
 
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes
+        
+    end
+
+    def self.find_movies_after_2002
+        Movie.where('release_date > 2002')
+    end
+
+    def update_with_attributes(title, release_date, director, lead, in_theaters)
+        Movie.update_attributes(title: title, release_date: release_date, director: director, lead: lead, in_theaters: in_theaters)
+    end
+
+    def self.update_all_titles(title)
+        Movie.update_all(title: title)
+    end
+
+    def self.delete_by_id(id)
+        Movie.destroy(id)
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
